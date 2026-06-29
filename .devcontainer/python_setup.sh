8a216fc3301db0879e5e1c41455078ccbc246738
izzat muhaimin <153487782+Izzat1098@users.noreply.github.com>
2026-06-29 00:38:23 +0000

    fix

--- .devcontainer/python_setup.sh
+++ .devcontainer/python_setup.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 # apt-get install -y -qq unzip curl wget git build-essential
 
 echo "Python installation..."
-sudo apt-get install -y python-is-python
+sudo apt-get install -y python-is-python3
 # use UV for dependency and venv management
 curl -LsSf https://astral.sh/uv/install.sh | sh
 cd $OLDPWD
